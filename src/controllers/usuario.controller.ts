fb2498c635fde40b117935948e9ce438978d5775
carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>
2021-11-15 08:51:43 -0500

    create password and crypt js
    Signed-off-by: carlosflunauc <90459421+carlosflunauc@users.noreply.github.com>

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -1,3 +1,4 @@
+import { service } from '@loopback/core';
 import {
   Count,
   CountSchema,
@@ -19,11 +20,15 @@ import {
 } from '@loopback/rest';
 import {Usuario} from '../models';
 import {UsuarioRepository} from '../repositories';
+import { AutenticacionService } from '../services';
 
 export class UsuarioController {
   constructor(
     @repository(UsuarioRepository)
     public usuarioRepository : UsuarioRepository,
+    @service(AutenticacionService)
+    public servicioAutenticacion : AutenticacionService
+
   ) {}
 
   @post('/usuarios')
@@ -44,7 +49,14 @@ export class UsuarioController {
     })
     usuario: Omit<Usuario, 'id'>,
   ): Promise<Usuario> {
-    return this.usuarioRepository.create(usuario);
+
+    let clave = this.servicioAutenticacion.GenerarClave();
+    let claveCifrada = this.servicioAutenticacion.CifrarClave(clave);
+    usuario.clave=claveCifrada;
+    let p = await this.usuarioRepository.create(usuario);
+
+
+    // notificacion al usuario por medio del email
   }
 
   @get('/usuarios/count')
